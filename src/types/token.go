988e9ee734d1121858e7d0c019c0e2cf09330c58
Michael Thornes <zytekaron@gmail.com>
2022-04-02 23:04:39 -0700

    remove r/w fields on token type

--- src/types/token.go
+++ src/types/token.go
@@ -5,12 +5,4 @@ type Token struct {
 	OwnerID   string `json:"owner_id" bson:"owner_id"`
 	CreatedAt Time   `json:"created_at" bson:"created_at"`
 	ExpiresAt Time   `json:"expires_at" bson:"expires_at"`
-
-	Read      bool     `json:"read" bson:"read"`
-	ReadLimit int      `json:"read_limit" bson:"read_limit"`
-	ReadReset Duration `json:"read_reset" bson:"read_reset"`
-
-	Write      bool     `json:"write" bson:"write"`
-	WriteLimit int      `json:"write_limit" bson:"write_limit"`
-	WriteReset Duration `json:"write_reset" bson:"write_reset"`
 }
--- src/types/updates/token.go
+++ src/types/updates/token.go
@@ -10,14 +10,6 @@ type Token struct {
 	OwnerID   optional.Optional[string]
 	CreatedAt optional.Optional[types.Time]
 	ExpiresAt optional.Optional[types.Time]
-
-	Read      optional.Optional[bool]
-	ReadLimit optional.Optional[int]
-	ReadReset optional.Optional[types.Duration]
-
-	Write      optional.Optional[bool]
-	WriteLimit optional.Optional[int]
-	WriteReset optional.Optional[types.Duration]
 }
 
 func (t *Token) WithLevel(ownerID string) *Token {
@@ -35,48 +27,10 @@ func (t *Token) WithExpiresAt(expiresAt types.Time) *Token {
 	return t
 }
 
-func (t *Token) WithRead(read bool) *Token {
-	t.Read = optional.Of(read)
-	return t
-}
-
-func (t *Token) WithReadLimit(readLimit int) *Token {
-	t.ReadLimit = optional.Of(readLimit)
-	return t
-}
-
-func (t *Token) WithReadReset(readReset types.Duration) *Token {
-	t.ReadReset = optional.Of(readReset)
-	return t
-}
-
-func (t *Token) WithWrite(write bool) *Token {
-	t.Write = optional.Of(write)
-	return t
-}
-
-func (t *Token) WithWriteLimit(writeLimit int) *Token {
-	t.WriteLimit = optional.Of(writeLimit)
-	return t
-}
-
-func (t *Token) WithWriteReset(writeReset types.Duration) *Token {
-	t.WriteReset = optional.Of(writeReset)
-	return t
-}
-
 func (t *Token) Apply(o *types.Token) {
 	o.OwnerID = t.OwnerID.OrElse(o.OwnerID)
 	o.CreatedAt = t.CreatedAt.OrElse(o.CreatedAt)
 	o.ExpiresAt = t.ExpiresAt.OrElse(o.ExpiresAt)
-
-	o.Read = t.Read.OrElse(o.Read)
-	o.ReadLimit = t.ReadLimit.OrElse(o.ReadLimit)
-	o.ReadReset = t.ReadReset.OrElse(o.ReadReset)
-
-	o.Write = t.Write.OrElse(o.Write)
-	o.WriteLimit = t.WriteLimit.OrElse(o.WriteLimit)
-	o.WriteReset = t.WriteReset.OrElse(o.WriteReset)
 }
 
 func (t *Token) MongoQuery() bson.M {
@@ -89,26 +43,6 @@ func (t *Token) MongoQuery() bson.M {
 	}
 	if t.ExpiresAt.IsPresent() {
 		set["expires_at"] = t.ExpiresAt.Get()
-	}
-
-	if t.Read.IsPresent() {
-		set["read"] = t.Read.Get()
-	}
-	if t.ReadLimit.IsPresent() {
-		set["read_limit"] = t.ReadLimit.Get()
-	}
-	if t.ReadReset.IsPresent() {
-		set["read_reset"] = t.ReadReset.Get()
-	}
-
-	if t.Write.IsPresent() {
-		set["write"] = t.Write.Get()
-	}
-	if t.WriteLimit.IsPresent() {
-		set["write_limit"] = t.WriteLimit.Get()
-	}
-	if t.WriteReset.IsPresent() {
-		set["write_reset"] = t.WriteReset.Get()
 	}
 
 	return bson.M{
